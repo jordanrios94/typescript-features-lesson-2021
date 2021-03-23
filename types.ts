07fb9834c9c958226d3c28d82e07ce083d8cec6e
jordanrios94 <jordanrios.software@gmail.com>
2021-03-23 11:20:33 +0000

    feat: types

--- types.ts
+++ types.ts
@@ -7,5 +7,15 @@ class Color { }
 const red = new Color();
 
 /*
+Type -> Easy to refer to the different properties + functions that a value has
+
+Types can be grouped into;
+  - Primitive Types
+  - Object Types
+
+Why do we care about types?
+  - Types are used by the Typescript Compiler to analyze our code for errors
+  - Types allow other engineers to understand what values are flowing around our codebase
+
 USE TYPES EVERYWHERE!
 */
